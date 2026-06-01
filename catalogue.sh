6ca44b25122d75c650145acef3bf1ce09452ae65
unknown <amalli101010@gmail.com>
2026-06-01 20:15:22 +0530

    internal commit

--- catalogue.sh
+++ catalogue.sh
@@ -94,6 +94,6 @@ dnf install mongodb-mongosh -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB client"
 
-mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+mongosh --host mongodb.rpop.online </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"
