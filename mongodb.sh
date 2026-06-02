47822fb5f39414df35db7405247fdf0d8e2b84de
unknown <amalli101010@gmail.com>
2026-06-02 14:34:35 +0530

    internal commit

--- mongodb.sh
+++ mongodb.sh
@@ -1,66 +1,22 @@
 #!/bin/bash
 
-ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
+check_root
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
-if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 # you can give other than 0
-else
-    echo "You are root user"
-fi # fi means reverse of if, indicating condition end
-
-for package in $@
-do
-    dnf list installed $package &>> $LOGFILE #check installed or not
-    if [ $? -ne 0 ] #if not installed
-    then
-        dnf install $package -y &>> $LOGFILE # install the package
-        VALIDATE $? "Installation of $package" # validate
-    else
-        echo -e "$package is already installed ... $Y SKIPPING $N"
-    fi
-done
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-
-VALIDATE $? "Copied MongoDB Repo"
-
-dnf install mongodb-org -y &>> $LOGFILE
-
+dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "Installing MongoDB"
 
-systemctl enable mongod &>> $LOGFILE
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
 
-VALIDATE $? "Enabling MongoDB"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl start mongod &>> $LOGFILE
-
-VALIDATE $? "Starting MongoDB"
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
-
-VALIDATE $? "Remote access to MongoDB"
-
-systemctl restart mongod &>> $LOGFILE
-
+systemctl restart mongod
 VALIDATE $? "Restarting MongoDB"
+
+print_total_time
